2fe8c763e0bdbf8f245c7ce222e6da9332832290
Sandro Torres <sandrotorres.ti@gmail.com>
2022-10-27 17:21:07 -0300

    Sobre jsx 02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = "Sandro";
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <h1>Olá React!</h1>
-      <p>Desvendando React...</p>
+      <p>Olá, {newName}</p>
+      <p> Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Via.Placeholder" />
     </div>
   )
 }
